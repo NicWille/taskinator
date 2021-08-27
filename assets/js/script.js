21e742745b8ce254862c994a07865ddb2adf445e
NicWille <nic.wille1020@yahoo.com>
2021-08-27 10:53:23 -0700

    working btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
 
-buttonEl.addEventListener("click", function(){
-    alert("button clicked?")
-})
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var taskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", taskHandler);
+
