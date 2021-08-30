a2400dbebb728780edf65094866bf779c6a0a0a8
NicWille <nic.wille1020@yahoo.com>
2021-08-30 10:36:11 -0700

    disabled the event default

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-
-var buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskHandler = function() {
+var createTaskHandler = function() {
+    // console.log(event)
+    event.preventDefault()
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", taskHandler);
+formEl.addEventListener("submit", createTaskHandler)
 
