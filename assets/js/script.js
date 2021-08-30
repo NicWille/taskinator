34b46bc60d6721fe1cd823f4403db43cb77c4abd
NicWille <nic.wille1020@yahoo.com>
2021-08-30 14:15:40 -0700

    ux quality updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,15 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskNameInput) {
+        alert("Fill out the form first!")
+        return false
+    }
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+    formEl.reset()
 
     createTaskEl(taskDataObj);
 }
